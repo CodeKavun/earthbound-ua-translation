d2e32000e93048f81fc2e51cf42bf1480cc839d7
PanDmytryk <dimacapny22@gmail.com>
2025-07-23 17:50:22 +0200

    Complete translation of Twoson, PRV and HH Village

--- cyrr2eng.py
+++ cyrr2eng.py
@@ -1,13 +1,13 @@
 from charconf import CYRILLIC_TO_LATIN
 
 
-user_input = input("Enter cyrrilic text to interpretate: ")
+while True:
+    user_input = input("Enter cyrrilic text to interpretate: ")
 
-result = ""
-for char in user_input:
-    if (char in CYRILLIC_TO_LATIN): result += CYRILLIC_TO_LATIN[char]
-    else: result += char
+    result = ""
+    for char in user_input:
+        if (char in CYRILLIC_TO_LATIN): result += CYRILLIC_TO_LATIN[char]
+        else: result += char
 
 
-print(result)
-input()
+    print(result)
